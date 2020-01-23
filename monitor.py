f18d2a28a4f6a2a4bcce542adb275dc7f7d68d82
Max Zintel <Mackenzie.Zintel@partner.bmwgroup.com>
2020-01-23 16:51:29 -0500

    Finally completely done. A little rough around the edges, but it works.

--- monitor.py
+++ monitor.py
@@ -4,54 +4,92 @@ import requests
 import json
 
 ec2 = boto3.client('ec2')
+ec2Resource = boto3.resource('ec2')
 
 def handler(event, context):
   ec2Id = event['detail']['EC2InstanceId']
+  base = 'clustername-worker-'
+  baseWild = 'clustername-worker-*'
+  matched = ec2.describe_instances(Filters=[
+    {
+      'Name': 'tag:Name',
+      'Values':
+        [
+          baseWild
+        ]
+    }
+  ])
 
-  if event['detail-type'] == 'EC2 Instance Launch Successful':
-    ec2Info = ec2.describe_instances(InstanceIds=[ec2Id])
-    ec2Ip = ec2Info['Reservations'][0]['Instances'][0]['PrivateIpAddress']
-    loadBalancer = switch_account_to_internal()
+  workerCount = len(matched["Reservations"])
+  for i in range(0, workerCount):
+    for instanceObj in matched["Reservations"]:
+      instanceID = str(instanceObj["Instances"][0]["InstanceId"])
+      custom = base + str(i)
+      ec2.create_tags(
+        DryRun=False,
+        Resources=[
+          instanceID
+        ],
+        Tags=[
+          {
+            'Key': 'Name',
+            'Value': str(custom)
+          }
+        ]
+      )
+      i+=1
+    else:
+      if event['detail-type'] == 'EC2 Instance Launch Successful':
+        ec2Info = ec2.describe_instances(InstanceIds=[ec2Id])
+        ec2Ip = ec2Info['Reservations'][0]['Instances'][0]['PrivateIpAddress']
+        loadBalancer = switch_account_to_internal()
 
-    if addEC2Instance(ec2Ip, loadBalancer):
-      sendSlackMessage(findEC2Name(ec2Info), ec2Ip, True)
-  elif event['detail-type'] == 'EC2 Instance Terminate Successful':
-    autoScaling = boto3.client('autoscaling')
-    autoScalingInfo = autoScaling.describe_auto_scaling_groups(AutoScalingGroupNames=[
-      'group name'
-      ]
-    )
-    currentInstancesInfo = autoScalingInfo['AutoScalingGroups'][0]['Instances']
-    currentInstances = []
-    for i in range(len(currentInstancesInfo)):
-      currentInstances.append(currentInstancesInfo[i]['InstanceId'])
+        if addEC2Instance(ec2Ip, loadBalancer):
+          sendSlackMessage(findEC2Name(ec2Info), ec2Ip, True)
+      elif event['detail-type'] == 'EC2 Instance Terminate Successful':
+        autoScaling = boto3.client('autoscaling')
+        autoScalingInfo = autoScaling.describe_auto_scaling_groups(AutoScalingGroupNames=[
+          'clustername-worker terraform-xxx'
+          ]
+        )
+        currentInstancesInfo = autoScalingInfo['AutoScalingGroups'][0]['Instances']
+        currentInstances = []
+        for i in range(len(currentInstancesInfo)):
+          currentInstances.append(currentInstancesInfo[i]['InstanceId'])
 
-    ec2Info = ec2.describe_instances(InstanceIds=currentInstances)
+        ec2Info = ec2.describe_instances(InstanceIds=currentInstances)
+        ec2Ips = []
 
-    ec2Ips = []
+        for ec2Instances in ec2Info['Reservations']:
+          for value in ec2Instances['Instances']:
+            ec2Ips.append(value['PrivateIpAddress'])
 
-    for ec2Instances in ec2Info['Reservations']:
-      for value in ec2Instances['Instances']:
-        ec2Ips.append(value['PrivateIpAddress'])
+        loadBalancer = switch_account_to_internal()
 
-    loadBalancer = switch_account_to_internal()
+        targetGroupHealthHTTP = loadBalancer.describe_target_health(TargetGroupArn=json.loads(os.environ['ARN_TARGET_GROUPS'])[0]['arn'])
+        targetGroupIps = []
 
-    targetGroupHealthHTTP = loadBalancer.describe_target_health(TargetGroupArn=json.loads(os.environ['ARN_TARGET_GROUPS'])[0]['arn'])
-    targetGroupIps = []
+        for value in targetGroupHealthHTTP['TargetHealthDescriptions']:
+          targetGroupIps.append(value['Target']['Id'])
 
-    for value in targetGroupHealthHTTP['TargetHealthDescriptions']:
-      targetGroupIps.append(value['Target']['Id'])
+        notMatchingIps = list(set(targetGroupIps) - set(ec2Ips))
 
-    notMatchingIps = list(set(targetGroupIps) - set(ec2Ips))
-
-    for ip in notMatchingIps:
-      if removeFromTargetGroup(ip, loadBalancer):
-        try:
-          sendSlackMessage(findEC2Name(ec2.describe_instances(InstanceIds=[ec2Id])), ip, False)
-        except:
-          sendSlackMessage('Could not find name', ip, False)
-
-  return 'successful'
+        for ip in notMatchingIps:
+          if removeFromTargetGroup(ip, loadBalancer):
+            try:
+              sendSlackMessage(findEC2Name(ec2.describe_instances(InstanceIds=[ec2Id])), ip, False)
+            except:
+              sendSlackMessage('Could not find name', ip, False)
+      user = os.environ['user']
+      passw = os.environ['passw']
+      url = os.environ['URL']
+      HEADERS = {'Content-Type':'application/json'}
+      print 'Sending api call to `%s`'% url
+      post = requests.post(url, headers=HEADERS, auth=(user, passw))
+      content = post.content
+      print(post.status_code)
+      print 'Call sent, response: `%s`'% content
+      return 'successful, worker tags edited: ' + str(workerCount)
 
 def addEC2Instance(ip, loadBalancer):
   targetGroups = json.loads(os.environ['ARN_TARGET_GROUPS'])
@@ -102,8 +140,8 @@ def removeFromTargetGroup(ip, loadBalancer):
 def switch_account_to_internal():
   return boto3.client(
     'elbv2',
-    aws_access_key_id=os.environ['BASIC_ACCOUNT_ACCESS_KEY_ID'],
-    aws_secret_access_key=os.environ['BASIC_ACCOUNT_SECRET_ACCESS_KEY']
+    aws_access_key_id=os.environ['ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['SECRET_ACCESS_KEY']
   )
 
 def sendSlackMessage(nameOfInstance, ip, didCreate):
@@ -114,60 +152,19 @@ def sendSlackMessage(nameOfInstance, ip, didCreate):
     slackPayload = json.load(f)
 
   if didCreate:
-    slackPayload['text'] = 'Autoscaling Event Captured - Target Created :white_check_mark:'
-    slackPayload['attachments'][0]['blocks'][0]['text']['text'] = '*Cluster:* name \n *Instance:* `%s` \n *IP:* `%s`' %(nameOfInstance, ip)
+    slackPayload['text'] = ':aws: Autoscaling Event Captured - Target Created :white_check_mark:'
+    slackPayload['attachments'][0]['blocks'][0]['text']['text'] = '*Cluster:* clustername \n *Instance:* `%s` \n *IP:* `%s`' %(nameOfInstance, ip)
   else:
-    slackPayload['text'] = 'Autoscaling Event Captured - Target Deleted :downvote:'
-    slackPayload['attachments'][0]['blocks'][0]['text']['text'] = '*Cluster:* name \n *Instance:* `%s` \n *IP:* `%s`' %(nameOfInstance, ip)
+    slackPayload['text'] = ':aws: Autoscaling Event Captured - Target Deleted :downvote:'
+    slackPayload['attachments'][0]['blocks'][0]['text']['text'] = '*Cluster:* clustername \n *Instance:* `%s` \n *IP:* `%s`' %(nameOfInstance, ip)
 
   print 'Sending slack message'
   r = requests.post(url=os.environ['SLACK_ENDPOINT'], data=json.dumps(slackPayload), headers=headers)
   print 'Slack response: %s' % str(r)
 
-  BAMBOO_USERPASS = os.environ['BAMBOO_USERPASS']
-  url = os.environ['URL']
-  HEADERS = {'Authorization':'Basic `%s`'% BAMBOO_USERPASS}
-  print 'Sending api call to `%s`'% url
-  response = requests.Get(url,headers=HEADERS)
-  content = response.content
-  print 'Call sent, response: `%s`'% content
-
 def findEC2Name(ec2Info):
-  renameWorkers
   tags = ec2Info['Reservations'][0]['Instances'][0]['Tags']
+
   for tag in tags:
     if tag['Key'] == 'Name':
       return tag['Value']
-
-def renameWorkers():
-  base = 'test'
-  baseWild = 'test*'
-  workers = ec2.describe_instances(Filters=[
-    {
-      'Name': 'tag:Name',
-      'Values':
-        [
-          baseWild
-        ]
-    }
-  ])
-  workerCount = len(matched["Reservations"])
-  for i in range(0, workerCount):
-    for instanceObj in matched["Reservations"]:
-      instanceID = str(instanceObj["Instances"][0]["InstanceId"])
-      custom = base + str(i)
-      ec2.create_tags(
-        DryRun=False,
-        Resources=[
-          instanceID
-        ],
-        Tags=[
-          {
-            'Key': 'Name',
-            'Value': str(custom)
-          }
-        ]
-      )
-      i+=1
-    else:
-      return 'successful, ' + str(workerCount)
